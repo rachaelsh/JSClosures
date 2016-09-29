41d8c30448037c432f4091d4e024535c951006d1
rachaelsh <rachael.s.harlow@gmail.com>
2016-09-29 11:09:19 -0600

    finished problems

--- ccPractice.js
+++ ccPractice.js
@@ -3,15 +3,27 @@ var someFunc, countup, counter, otherFunc;
 // Closures
 
 // 1. Create a function called someFunc that takes in a number and returns the definition of another anonymous function. That anonymous function will take in a second number and will return the sum of both numbers.
+var someFunc = function(num1){
+  return function(num2){
+    return num1 + num2;
+  };
+};
+
+var adding = someFunc();
+
+
 
 // Invoke the someFunc function, and pass in a number value (Let's use number 9) as the argument. Assign the response to another variable called otherFunc.
 
+var otherFunc = adding(9);
+
 // When you are done, we can invoke the otherFunc function and pass in another number value as its argument. And we can do this as often as we like. Go ahead and uncomment out the following invocations.
 
-// otherFunc(10) // 19
-// otherFunc(26) // 35
-// otherFunc(100) // 109
-// otherFunc(1) // 10
+
+// otherFunc(10); // 19
+// otherFunc(26); // 35
+// otherFunc(100); // 109
+// otherFunc(1); // 10
 
 // You have just created your first closure. What you have essentially done is contained a fixed inputted number value in the local someFunc scope, while allowing the user to run an internal function as often as he likes using any valid number argument.
 
@@ -22,14 +34,26 @@ var someFunc, countup, counter, otherFunc;
 
 // Create a function called countSet, no parameters needed, and set a new variable (call it countup) to equal 0. Have the function return an anonymous function that returns the countup variable after being incremented.
 
+var countSet = function(){
+  var countup = 0;
+  return function(){
+    countup++;
+    return countup;
+  };
+};
+
+
+
 // Assign the invocation of the countSet function to a new variable. Let's call it counter.
 
-// Then whenever you need to count up, you can invoke the counter function. Go ahead and uncomment out the following invocations.
+var counter = countSet();
 
-// counter()//1
-// counter()//2
-// counter()//3
-// counter()//4
+// Then whenever you need to count up, you can invoke the counter function. Go ahead and uncomment out the following invocations.
+//
+counter();//1
+counter();//2
+counter();//3
+counter();//4
 
 module.exports = {
   someFunc: someFunc,
